9a1b2b0f0c153b9f1df8b2f50043fd51c5f866cb
Joanna Lau <118241363+joannalauu@users.noreply.github.com>
2026-07-02 21:41:50 -0400

    fix lint

--- backend/python/app/routers/__init__.py
+++ backend/python/app/routers/__init__.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
-from . import entity_routes, event_routes, event_type_routes, registration_routes, simple_entity_routes, user_routes
+from . import (
+    entity_routes,
+    event_routes,
+    event_type_routes,
+    registration_routes,
+    simple_entity_routes,
+    user_routes,
+)
 
 
 def init_app(app: FastAPI) -> None:
